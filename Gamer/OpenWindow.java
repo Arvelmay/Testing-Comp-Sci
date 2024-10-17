d112a735bd569b4fee24d58c316140b3b038a334
Arvelmay <123535960+Arvelmay@users.noreply.github.com>
2024-10-17 10:05:15 -0500

    trying out JFrame

---
+++ Gamer/OpenWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class OpenWindow
+{
+    public static void main(String[] args)
+    {
+        JFrame framed = new JFrame();
+        framed.add(child);
+    }
+}
